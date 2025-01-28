c0ba1f77925dba2d4a1a5035cb3710926591a054
Alex Akbar Fallah Chamasmani <alexfallah@gmail.com>
2025-01-28 13:31:04 -0800

    We added the area

---
+++ index.js
@@ -0,0 +1,4 @@
+//The area in which the plants are contained is circular, with a radius of 5 meters.
+//The formula for calculating the area of a circle is PI multiplied by the radius, squared:
+let pi = 3.14
+let area = pi * radius * radius
